a8e6e032c01febb6a50379861978bd08e684ce24
Philip Oseikhuemen Inegbedion <philliposeikhuemen@gmail.com>
2025-08-24 22:31:40 +0100

    Adds script to describe the 'books' table schema.

--- task_4.sql
+++ task_4.sql
@@ -1,19 +1,14 @@
--- This script queries the INFORMATION_SCHEMA to show details about the 'books' table.
-
--- Use the 'alx_book_store' database.
 USE alx_book_store;
 
--- Select specific columns from the INFORMATION_SCHEMA.COLUMNS view
--- to get metadata about the columns in the 'books' table.
-SELECT
+SELECT 
     COLUMN_NAME,
     COLUMN_TYPE,
     IS_NULLABLE,
     COLUMN_KEY,
     COLUMN_DEFAULT,
     EXTRA
-FROM
+FROM 
     INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_NAME = 'books'
+WHERE 
+    TABLE_NAME = 'Books'
     AND TABLE_SCHEMA = 'alx_book_store';
